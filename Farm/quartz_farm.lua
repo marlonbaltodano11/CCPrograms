4bf9fcf6d20a8f61dc40bca94c90d180a04dbbf8
Marlon Baltodano <marlonbaltodano11@gmail.com>
2024-06-04 01:35:57 -0600

    Granja de cuarzo certerus
    Se creó la primera versión de la granja de cuarzo certerus usando un autómata

---
+++ Farm/quartz_farm.lua
@@ -0,0 +1,181 @@
+
+local quartz_block = {"ae2:flawed_budding_quartz", "ae2:chipped_budding_quartz", "ae2:damaged_budding_quartz", "ae2:quartz_block"}
+local machine = "ae2:growth_accelerator"
+
+local max_x = 10
+local max_y = 10
+
+local facing = "up"
+local invert = false
+
+
+local function reset()
+    facing = "up"
+    invert = false
+end
+
+
+local function makeTurn()
+    if not invert then
+        if facing == "up" then
+            turtle.turnRight()
+            turtle.forward()
+            turtle.turnRight()
+            facing = "down"
+        else
+            turtle.turnLeft()
+            turtle.forward()
+            turtle.turnLeft()
+            facing = "up"
+        end
+    else
+        if facing == "up" then
+            turtle.turnLeft()
+            turtle.forward()
+            turtle.turnLeft()
+            facing = "down"
+        else
+            turtle.turnRight()
+            turtle.forward()
+            turtle.turnRight()
+            facing = "up"
+        end
+    end
+end
+
+local function turnAround()
+    turtle.turnRight()
+    turtle.turnRight()
+
+    if facing == "up" then
+        facing = "down"
+    else
+        facing = "up"
+    end
+end
+turtle.turnAround = turnAround
+
+local function replenish()
+    turtle.turnAround()
+
+    for _ = 1, 2, 1 do
+        turtle.suck()
+    end
+
+    local fuelSlot = 1
+
+    for i = 1, 16, 1 do
+        local name = turtle.getItemDetail(i)["name"]
+
+        if name == "minecraft:charcoal" then
+            fuelSlot = i
+            break
+        end
+    end
+
+    turtle.select(fuelSlot)
+    turtle.refuel()
+end
+
+local function findQuartzBlocks()
+    local quartzSlot = 1
+    
+    for i = 1, 16, 1 do
+        local name = turtle.getItemDetail(i)["name"]
+
+        if name == "minecraft:charcoal" then
+            quartzSlot = i
+            break
+        end
+    end
+
+    turtle.select(quartzSlot)
+end
+
+local function collectQuartz()
+    for x = 1, max_x - 1, 1 do
+        for y = 1, max_y - 1, 1 do
+            turtle.forward()
+
+            local isOccupied, data = turtle.inspectDown()
+
+            if isOccupied then
+
+                local isQuartzBlock = false
+
+                for _, v in pairs(quartz_block) do
+                    if v == data["name"] then
+                        isQuartzBlock = true
+                        break
+                    end
+                end
+
+                local isMachine = machine == data["name"]
+
+                if not isQuartzBlock and not isMachine then
+                    turtle.digDown()
+                end
+            end
+
+            if turtle.detect() and y < max_y - 1 then
+                turtle.dig()
+            end
+        end
+        makeTurn()
+    end
+end
+
+local function replantQuartz()
+    turtle.turnAround()
+    invert = true
+
+    for x = 1, max_x - 1, 1 do
+        for y = 1, max_y - 1, 1 do
+            turtle.forward()
+
+            local isOccupied, data = turtle.inspectDown()
+
+            if isOccupied then
+                local isMachine = machine == data["name"]
+
+                if not isMachine and data["name"] == "ae2:quartz_block" then
+                    turtle.digDown()
+                    turtle.placeDown()
+                else
+                    local isQuartzBlock = false
+
+                    for _, v in pairs(quartz_block) do
+                        if v == data["name"] then
+                            isQuartzBlock = true
+                            break
+                        end
+                    end
+
+                    if not isQuartzBlock and not isMachine then
+                        turtle.digDown()
+                    end
+                end
+            end
+            
+            if turtle.detect() and y < max_y - 1 then
+                turtle.dig()
+            end
+        end
+        makeTurn()
+    end
+
+    turtle.turnAround()
+end
+
+local function main()
+    while true do
+        replenish()
+        collectQuartz()
+        findQuartzBlocks()
+        replantQuartz()
+        reset()
+        os.sleep(20)
+    end
+end
+
+main()
